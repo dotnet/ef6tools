721bc51c910892c2dbe45bfbba36d0b26273f74f
Ryan Brandenburg <rybrande@microsoft.com>
2021-08-03 10:55:50 -0700

    Update to MIT license (#21)

--- src/EntityFramework/Core/Objects/ELinq/BindingContext.cs
+++ src/EntityFramework/Core/Objects/ELinq/BindingContext.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
 using CqtExpression = System.Data.Entity.Core.Common.CommandTrees.DbExpression;
 using LinqExpression = System.Linq.Expressions.Expression;
