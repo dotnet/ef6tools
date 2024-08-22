aeea6d2931ce17900e79c60a0c8abff34d77fe06
phil-allen-msft <phillipa@microsoft.com>
2024-08-22 12:35:26 -0700

    Redo removal

--- src/EntityFramework.SqlServer/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("EntityFramework.SqlServer")]
-[assembly: AssemblyDescription("EntityFramework.SqlServer.dll")]
-[assembly: AssemblyDefaultAlias("EntityFramework.SqlServer.dll")]
--- src/EntityFramework/Infrastructure/Interception/IDbMutableInterceptionContext`.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
-
-namespace System.Data.Entity.Infrastructure.Interception
-{
-    internal interface IDbMutableInterceptionContext<TResult> : IDbMutableInterceptionContext
-    {
-        new InterceptionContextMutableData<TResult> MutableData { get; }
-    }
-}
