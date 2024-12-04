7b6c88a3986e18c3977122cdc6eee4e03fcaf4b1
Phil Allen <phillipa@microsoft.com>
2024-12-04 12:45:19 -0800

    Generate "InsertionParameters.txt" to simplify build and release (#71)
    Generate "InsertionParameters.txt" to simplify build and release
    
    This includes:
    Switching to Nerdbank.Versioning, setting some base version numbers, and add to the build.
    Generate an InsertionParameters.txt to fill in some environment variables. This needs the version numbers created from Nerdbank.Versioning
    Generate a nuspec file to pack&push internally for consumption. This needs the version numbers created from Nerdbank.Versioning.
    Remove vestigial nuspec/package generation
    
    
    
    * Add CreateInsertionMetadata.proj (adapted from https://github.com/aspnet/librarymanager)
    
    * Make it easier to run the product after building with local VS 2022
    
    * Remove StyledHyperlink class
    
    It is unreferenced, and with modern versioning it seems to have some trouble being found by Generic.xaml during localization
    
    * Add generation of a nuspec file
    
    * Remove vestigial nuspec/package generation
    
    * Fix name to match "SingletonMsiName"
    
    * Remove 'readme' from nuspec for now

--- src/EFTools/SharedAssemblyInfo.cs
+++ src/EFTools/SharedAssemblyInfo.cs
@@ -11,21 +11,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-#if !BUILD_GENERATED_VERSION
-#if VS11
-[assembly: AssemblyVersion("11.2.0.0")]
-[assembly: AssemblyFileVersion("11.2.0.0")]
-#elif VS12
-[assembly: AssemblyVersion("12.0.0.0")]
-[assembly: AssemblyFileVersion("12.0.0.0")]
-#elif VS14
-[assembly: AssemblyVersion("14.0.0.0")]
-[assembly: AssemblyFileVersion("14.0.0.0")]
-#elif VS15
-[assembly: AssemblyVersion("15.0.0.0")]
-[assembly: AssemblyFileVersion("15.0.0.0")]
-#elif VS16 || VS17
-[assembly: AssemblyVersion("16.0.0.0")]
-[assembly: AssemblyFileVersion("16.0.0.0")]
-#endif
-#endif
